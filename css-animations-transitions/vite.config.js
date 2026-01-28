94ddad51672f06f38211d3f4618df04719914006
adriaan-glibert-development <info@adriaanglibert.be>
2026-01-28 11:43:34 +0100

    ✨ Add descriptions in HTML and Create CSS transitions

---
+++ css-animations-transitions/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import fs from 'fs';
+
+const pagesDir = resolve(__dirname, 'exercise');
+const input = {};
+
+for (const file of fs.readdirSync(pagesDir)) {
+    if (file.endsWith('.html')) {
+        const name = file.replace(/\.html$/, '');
+        input[name] = resolve(pagesDir, file);
+    }
+}
+
+export default defineConfig({
+    root: pagesDir, // serve files from the pages folder
+    build: {
+        rollupOptions: {
+            input
+        }
+    }
+});
