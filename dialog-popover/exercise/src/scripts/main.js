2374cac08ff6ba8c69236386099c3198d0e6af33
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2026-01-28 18:26:34 +0100

    feat(dialog-popover): add solutions

--- dialog-popover/exercise/src/scripts/main.js
+++ dialog-popover/exercise/src/scripts/main.js
@@ -1,3 +1,33 @@
-import '../styles/main.css';
+import "../styles/main.css";
 
 /* showModal, closeModal is beschikbaar op dialog elementen */
+document.querySelectorAll("[data-modal]").forEach((modalBtn) => {
+  modalBtn.addEventListener("click", () => {
+    const modalId = modalBtn.getAttribute("data-modal");
+    document.getElementById(modalId)?.showModal();
+  });
+});
+
+document.querySelectorAll("[data-close]").forEach((closeBtn) => {
+  closeBtn.addEventListener("click", () => {
+    const modalId = closeBtn.getAttribute("data-close");
+    document.getElementById(modalId)?.close();
+  });
+});
+
+/* Popover toggle */
+document.querySelectorAll("[data-popover]").forEach((popoverBtn) => {
+  const popoverId = popoverBtn.getAttribute("data-popover");
+  const popover = document.getElementById(popoverId);
+
+  popoverBtn.addEventListener("click", () => {
+    popover?.togglePopover();
+  });
+
+  popover?.addEventListener("toggle", () => {
+    popoverBtn.setAttribute(
+      "aria-expanded",
+      popover.matches(":popover-open") ? "true" : "false",
+    );
+  });
+});
