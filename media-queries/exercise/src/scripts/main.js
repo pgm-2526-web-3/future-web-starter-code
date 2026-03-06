85c15329ad0e5434ceaa6b2dc6f8357f7fa37ea0
Sjouwke Van Parys <sjouwkevanparys@hotmail.com>
2026-03-06 15:52:45 +0100

    feat(wip): media-queries

--- media-queries/exercise/src/scripts/main.js
+++ media-queries/exercise/src/scripts/main.js
@@ -1,1 +1,18 @@
-import '../styles/main.css';
+import "../styles/main.css";
+
+const button = document.getElementById("toggle-color-mode");
+const root = document.documentElement;
+
+button.addEventListener("click", () => {
+  const current = root.dataset.theme;
+
+  root.setAttribute("data-theme", current === "dark" ? "light" : "dark");
+
+  localStorage.setItem("theme", root.dataset.theme);
+});
+
+const saved = localStorage.getItem("theme");
+
+if (saved) {
+  document.documentElement.setAttribute("data-theme", saved);
+}
